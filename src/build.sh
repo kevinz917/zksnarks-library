e70a55bb052b60da1f0214088953f18d1b85c368
Kevin Z <zkw917@gmail.com>
2021-10-29 11:40:32 -0400

    Updage bash script

--- src/build.sh
+++ src/build.sh
@@ -1,28 +1,48 @@
-echo "Building circuit" &&
-circom main.circom --r1cs --wasm --sym &&
+#!/bin/bash
 
-snarkjs zkey new main.r1cs ./powerOfTau/powersOfTau28_hez_final_15.ptau circuit_0000.zkey &&
-snarkjs zkey contribute circuit_0000.zkey circuit_final.zkey &&
-snarkjs zkey export verificationkey circuit_final.zkey verification_key.json &&
+function buildCircuit() {
+    echo "Building circuit" &&
+        circom main.circom --r1cs --wasm --sym &&
+        snarkjs zkey new main.r1cs ./powerOfTau/powersOfTau28_hez_final_15.ptau circuit_0000.zkey &&
+        snarkjs zkey contribute circuit_0000.zkey circuit_final.zkey &&
+        snarkjs zkey export verificationkey circuit_final.zkey verification_key.json &&
+        snarkjs wtns calculate main.wasm input.json witness.wtns &&
+        snarkjs groth16 prove circuit_final.zkey witness.wtns proof.json public.json &&
+        snarkjs groth16 verify verification_key.json public.json proof.json &&
+        snarkjs zkey export solidityverifier circuit_final.zkey verifier.sol &&
+        snarkjs generatecall >contractInput.js &&
+        cleanUp &&
+        echo "Finished compiling circuit!"
 
-snarkjs wtns calculate main.wasm input.json witness.wtns &&
+}
 
-snarkjs groth16 prove circuit_final.zkey witness.wtns proof.json public.json &&
+function cleanUp() {
+    echo "Clearing files"
+    rm main.r1cs
+    rm main.wasm
+    rm verification_key.json
+    rm main.sym
+    rm witness.wtns
+    rm circuit_0000.zkey
+    rm circuit_final.zkey
+    rm proof.json
+    echo "Finished cleaning files!"
+    date
 
-snarkjs groth16 verify verification_key.json public.json proof.json &&
+}
 
-snarkjs zkey export solidityverifier circuit_final.zkey verifier.sol &&
+no_args="true"
+while getopts ":c" opt; do
+    case $opt in
+    c)
+        cleanUp
+        ;;
 
-snarkjs generatecall > contractInput.js &&
+    esac
+    no_args="false"
+done
 
-echo "clearing files"
-rm main.r1cs
-rm main.wasm
-rm verification_key.json
-rm main.sym
-rm witness.wtns
-rm circuit_0000.zkey
-rm circuit_final.zkey
-
-echo "Finished!" &&
-date
+[[ "$no_args" == "true" ]] && {
+    buildCircuit
+    exit 1
+}
